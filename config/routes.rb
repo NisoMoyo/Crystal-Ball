6996af5545211282bf29ae267f872278f0c4ce8a
NisoMoyo <nmoyo@chicagobooth.edu>
2021-03-24 09:14:16 +0000

    Routes complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,19 @@
 Rails.application.routes.draw do
 
+#homepage
+get("/homepage/", { :controller => "application", :action => "homepage" })
+
+#1
+get("/consultant/new/", { :controller => "application", :action => "blank_consultant_form" })
+get("/consultant/results/", { :controller => "application", :action => "calculate_consultant" })
+
+#2
+get("/accountant/new/", { :controller => "application", :action => "blank_accountant_form" })
+get("/accountant/results/", { :controller => "application", :action => "calculate_accountant" })
+
+#3
+get("/developer/new/", { :controller => "application", :action => "blank_developer_form" })
+get("/developer/results/", { :controller => "application", :action => "calculate_developer" })
 
 
 end
